bbcfce09e6ecd19e5d1a50d482a1b2627427172e
dr-nemesis-96 <103036804+dr-nemesis-96@users.noreply.github.com>
2023-02-16 05:22:08 -0500

    Update nordify.sh

--- nordify.sh
+++ nordify.sh
@@ -1,21 +1,24 @@
-sudo pacman -S --needed wget dconf sassc gtk-engines gtk-engine-murrine
+sudo pacman -S --needed wget dconf sassc gtk-engines gtk-engine-murrine sudo git typescript make
+
 wget -qO- https://git.io/papirus-icon-theme-install | sh
-sudo rm -rf /usr/share/gnome-shell/extensions/
+sudo rm -rf /usr/share/gnome-shell/extensions/*
 mkdir ~/.themes
 cd ~/.themes
 wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/whitesur-nord-custom.tar.gz
 bsdtar -xf ./whitesur-nord-custom.tar.gz
 rm ./whitesur-nord-custom.tar.gz
 
-# mkdir /usr/share/gnome-shell/extentions
-mkdir ~/extensions
-cd ~/extensions
-wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/extentions.tar.gz
-bsdtar -xf ./extentions.tar.gz
-sudo rm ./extentions.tar.gz
-cp -R ~/extentions /usr/share/gnome-shell/
+wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
+chmod +x gnome-shell-extension-installer
+./gnome-shell-extension-installer 19 1319 307 5222 5338 1514 3193 615 4135
+rm ./gnome-shell-extention-installer
 
 cd ~
 wget https://raw.githubusercontent.com/dr-nemesis-96/whitesur-nord-custom/main/whitesur-nord-custom
+#POP SHELL
+git clone https://github.com/pop-os/shell.git
+cd shell
+make local-install
+#END OF POP SHELL
 dconf load / < whitesur-nord-custom
 rm ./whitesur-nord-custom
