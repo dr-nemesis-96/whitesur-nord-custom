1962d378864d311e7969811c0fc26a97aa2d66ac
dr-nemesis-96 <103036804+dr-nemesis-96@users.noreply.github.com>
2023-02-16 07:39:14 -0500

    Update nordify.sh

--- nordify.sh
+++ nordify.sh
@@ -1,7 +1,7 @@
 sudo pacman -S --needed wget dconf sassc gtk-engines gtk-engine-murrine sudo git typescript make
 
 wget -qO- https://git.io/papirus-icon-theme-install | sh
-sudo rm -rf /usr/share/gnome-shell/extensions/*
+# sudo rm -rf /usr/share/gnome-shell/extensions/*
 mkdir ~/.themes
 cd ~/.themes
 wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/whitesur-nord-custom.tar.gz
@@ -16,9 +16,13 @@ rm ./gnome-shell-extention-installer
 cd ~
 wget https://raw.githubusercontent.com/dr-nemesis-96/whitesur-nord-custom/main/whitesur-nord-custom
 #POP SHELL
-#git clone https://github.com/pop-os/shell.git
-#cd shell
-#make local-install
+# git clone https://github.com/pop-os/shell.git
+# cd shell
+# make local-install
 #END OF POP SHELL
+
 dconf load / < whitesur-nord-custom
 rm ./whitesur-nord-custom
+cd ~/Downloads
+wget https://github.com/dr-nemesis-96/whitesur-nord-custom/blob/main/10806048.jpg?raw=true
+gsettings set org.gnome.desktop.background picture-uri file:///$HOME/10806048.jpg
