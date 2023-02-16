2984daa1731f831ade7d6612f9e1df086ab5e17f
dr-nemesis-96 <103036804+dr-nemesis-96@users.noreply.github.com>
2023-02-16 01:37:09 -0500

    Add files via upload

---
+++ nordify.sh
@@ -0,0 +1,17 @@
+sudo pacman -S --needed wget dconf
+wget -qO- https://git.io/papirus-icon-theme-install | sh
+sudo rm -rf /usr/share/gnome-shell/extensions/*
+mkdir ~/.themes
+cd ~/.themes
+wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/whitesur-nord-custom.tar.gz
+bsdtar -xf ./whitesur-nord-custom.tar.gz
+rm ./whitesur-nord-custom.tar.gz
+
+cd ~/.local/share/gnome-shell/extentions/
+wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/extentions.tar.gz
+bsdtar -xf ./extentions.tar.gz
+rn ./extentions.tar.gz
+
+wget https://raw.githubusercontent.com/dr-nemesis-96/whitesur-nord-custom/main/whitesur-nord-custom
+dconf load / < whitesur-nord-custom
+rm ./whitesur-nord-custom
