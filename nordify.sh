9729f63f17f163cb66e46b680309d0c60464ad36
dr-nemesis-96 <103036804+dr-nemesis-96@users.noreply.github.com>
2023-02-16 04:39:54 -0500

    Update nordify.sh

--- nordify.sh
+++ nordify.sh
@@ -1,6 +1,6 @@
 sudo pacman -S --needed wget dconf sassc gtk-engines gtk-engine-murrine
 wget -qO- https://git.io/papirus-icon-theme-install | sh
-sudo rm -rf /usr/share/gnome-shell/extensions/*
+sudo rm -rf /usr/share/gnome-shell/extensions/
 mkdir ~/.themes
 cd ~/.themes
 wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/whitesur-nord-custom.tar.gz
@@ -8,9 +8,10 @@ bsdtar -xf ./whitesur-nord-custom.tar.gz
 rm ./whitesur-nord-custom.tar.gz
 
 # mkdir /usr/share/gnome-shell/extentions
-cd /usr/share/gnome-shell/extentions/
-sudo wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/extentions.tar.gz
-sudo bsdtar -xf ./extentions.tar.gz
+cd ~
+wget https://github.com/dr-nemesis-96/whitesur-nord-custom/raw/main/extentions.tar.gz
+bsdtar -xf ./extentions.tar.gz
+cp -R ./extentions /usr/share/gnome-shell/
 sudo rm ./extentions.tar.gz
 
 cd ~
